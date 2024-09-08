2df6f6341bc7d064348a58885b888be5577de1c2
hakan.basturknewinn.com <forxynet@gmail.com>
2024-09-09 00:04:05 +0300

    updated app with newproject

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,15 @@
+import Input from './input';
+
+export default function NewProject() {
+    return <div>
+        <menu>
+            <li><button>Cancel</button></li>
+            <li><button>Save</button></li>
+        </menu>
+        <div>
+            <Input label="Title" />           
+            <Input label="Description" textarea />           
+            <Input label="Due time" />           
+        </div>
+    </div>
+}
