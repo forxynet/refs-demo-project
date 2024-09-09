3a77f14e41d01f99be5a5cd45dacac6a88c4bd24
hakan.basturknewinn.com <forxynet@gmail.com>
2024-09-09 17:27:04 +0300

    added model

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,50 @@
+import { useRef } from 'react';
 import Input from './input';
 
-export default function NewProject() {
-    return <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
+export default function NewProject({onAdd}) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() { 
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        if (enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim === ''
+        ) {
+                
+          }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        });
+    }
+
+    return <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
+            <li>
+                <button className='text-stone-800 hover:text-stone-950'
+                >
+                    Cancel
+                </button>
+            </li>
+            <li>
+                <button
+                    className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+                    onClick={handleSave}>
+                    Save
+                </button>
+            </li>
         </menu>
         <div>
-            <Input label="Title" />           
-            <Input label="Description" textarea />           
-            <Input label="Due time" />           
+            <Input type='text' ref={title} label="Title" />
+            <Input ref={description} label="Description" textarea />
+            <Input type="date" ref={dueDate} label="Due time" />
         </div>
     </div>
 }
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,6 +1,16 @@
-export default function Input({label, textarea, ...props}) {
-    return <p>
-        <label>{label}</label>
-        {textarea ? <textarea {...props} /> : <input {...props} />}
+import { forwardRef } from "react";
+
+const Input = forwardRef(function ({ label, textarea, ...props }, ref) {
+    const classes = "w-full p-1 border-b-2 rounded border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-blue-600";
+
+    return <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+        {textarea ? (
+            <textarea ref={ref} className={classes} {...props} />
+        ) : (
+            <input ref={ref} className={classes} {...props} />)
+        }
     </p>
-}
+});
+
+export default Input;
