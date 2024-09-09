d4967709f4e7ae101f3128459eb8b5a19b82baaa
hakan.basturknewinn.com <forxynet@gmail.com>
2024-09-09 20:50:25 +0300

    addet delete function

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,31 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function Modal({ children }) {
-    return createPortal(
-        <dialog>{children}</dialog>,
-        document.getElementsById("modal-root")
-    );
-}
+import Button from './Button.jsx';
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md border-t-2 border-blue-300 focus:outline-none focus:border-blue-600"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  );
+});
+
+export default Modal;
